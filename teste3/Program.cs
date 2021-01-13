701bc870b5c4654ed7babb812e057689d51081e7
unknown <andressasantosleao_cia@hotmail.com>
2021-01-13 11:33:50 -0300

    first commit

---
+++ teste3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace teste3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int N;
+                        
+            string[] recebe = { Console.ReadLine() };
+            // Console.WriteLine($" O numero digitado é {recebe}");
+
+            foreach (var value in recebe)
+            {
+                bool sucesso = Int32.TryParse(value, out N);
+
+                if (sucesso)
+                {
+                    //Console.WriteLine($" O numero digitado é {N}");
+                    for( double i=1; i<= N; i++)
+                    {
+                        double iquadrado=2, icubo=3;
+                        
+                        Console.WriteLine($"{i} {iquadrado = Math.Pow(i, 2)} {icubo = Math.Pow(i, 3)}\n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($" O numero digitado é invalido");
+                }
+                
+            }
+
+           
+
+            
+        }
+    }
+}
